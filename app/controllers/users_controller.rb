2839033fae89d9cfe3904c5329787cdcd5a9f450
Kahlin92 <kahlin.naidoo@kellogg.northwestern.edu>
2022-03-13 00:49:49 +0000

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params["id"])
+  end
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(params["user"])
-    @user.save
-    redirect_to "/"
-  end
+plain_text_password = params["user"]["password"]
+@user = User.new(params["user"])
+@user.password = BCrypt::Password.create(plain_text_password)
+@user.save
+redirect_to "/users/#{@user.id}"
+end
 end
